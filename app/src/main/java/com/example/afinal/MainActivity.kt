cc5358797941cda36c3b9a37d6636d17954d91f4
gmitchell155 <gmitchell15@my.apsu.edu>
2022-04-20 16:08:49 -0500

    commit binding, first html file, options menu

---
+++ app/src/main/java/com/example/afinal/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.afinal
+
+import android.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebView
+import com.example.afinal.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myWebView: WebView = findViewById(R.id.test)
+        myWebView.loadUrl("file:///android_asset/index.html")
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.developer_navigation_menu){
+            //displays acknowledgement message
+            val builder = AlertDialog.Builder(binding.root.context)
+                .setTitle("About the Developer")
+                .setMessage("${R.string.developer_name}\n ${R.string.app_name}")
+                .setPositiveButton("ok", null)
+                .show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
